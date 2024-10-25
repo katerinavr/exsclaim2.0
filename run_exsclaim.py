83040b1baa6cb51a05b1d1f2013ab17b7896a31b
katerinavr <katerina_vriza@yahoo.gr>
2024-10-25 15:00:44 -0500

    clean code

--- run_exsclaim.py
+++ run_exsclaim.py
@@ -4,6 +4,6 @@ from exsclaim import tool
 from exsclaim.pipeline import Pipeline
 
 
-test_pipeline = Pipeline('./query/nature-ESEM.json') #(test_json)
+test_pipeline = Pipeline('./query/nature-ESEM.json')
 results = test_pipeline.run(caption_distributor=True,
         journal_scraper=True, figure_separator=True, html_scraper=False)
