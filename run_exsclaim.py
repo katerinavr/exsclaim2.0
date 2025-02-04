af879ce5e59efb55407dc547ff4731fe18f3df9a
katerinavr <katerina_vriza@yahoo.gr>
2025-02-04 14:37:16 -0600

    run_exsclaim.py

--- run_exsclaim.py
+++ run_exsclaim.py
@@ -1,31 +1,35 @@
-<<<<<<< HEAD
-=======
-from exsclaim import journal
-from exsclaim import pipeline
-from exsclaim import tool
->>>>>>> ad843cee0eb4e59ae1bc9cb0b4ed08b64a3370fa
 from exsclaim.pipeline import Pipeline
 
+
 test_json =  {
-    "name": "html-ECPs",
 
-    "pdf_folder": "pdf_files" ,
+    "name": "nature-nano",
 
-<<<<<<< HEAD
-     "llm": "gpt-4o",
-=======
-     "llm": "gpt-3.5-turbo",
->>>>>>> ad843cee0eb4e59ae1bc9cb0b4ed08b64a3370fa
+    "journal_family": "nature",
 
-    "openai_API": YOUR_OPENAI_API ,
-    "save_format": ["boxes", "save_subfigures", "csv"],
+    "maximum_scraped": 3,
+
+    "sortby": 'relevant',
+
+    "llm": "gpt-4o",
+
+    "openai_API": YOUR_OPENAI_API_KEY ,
+
+    "query":
+
+    { "search_field_1":
+
+        {   "term":"Ag nanoparticle",
+
+            "synonyms":["Ag nanoparticles", "silver nanoparticle", "silver nanoparticle", "nanoparticles of silver", "AgNPs", "AgNP", "Ag NPs", "silver NPs", "silver NP"] } },
+
+    "open": True,
+
+    "save_format": ["boxes", "save_subfigures" ],
 
     "logging": ["print", "exsclaim.log"]
     }
+
 test_pipeline = Pipeline(test_json)#'./query/nature-ESEM.json')
 results = test_pipeline.run(caption_distributor=True,
-<<<<<<< HEAD
         journal_scraper=True, figure_separator=True, pdf_scraper=False)
-=======
-        journal_scraper=False, figure_separator=True, pdf_scraper=True)
->>>>>>> ad843cee0eb4e59ae1bc9cb0b4ed08b64a3370fa
